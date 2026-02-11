3ec25b764da41d8bfd735f3dc65e683acbe8ce8e
TinGoll <roma.oliynyk83@gmail.com>
2026-02-11 21:09:03 +0300

    update components

--- tin-craft/src/renderer/src/components/buttons/Button.tsx
+++ tin-craft/src/renderer/src/components/buttons/Button.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from 'react'
 import styles from './Button.module.css'
 import clsx from 'clsx'
 
-type ButtonVariant = 'default' | 'primary'
+type ButtonVariant = 'default' | 'primary' | 'secondary' | 'danger'
 
 type Props = React.DetailedHTMLProps<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -10,8 +10,30 @@ type Props = React.DetailedHTMLProps<
 > & {
   icon?: ReactNode
   variant?: ButtonVariant
+  loading?: boolean
 }
 
-export const Button: FC<Props> = ({ variant = 'default', className, ...props }) => {
-  return <button {...props} className={clsx(styles.mcButton, styles[variant], className)} />
+export const Button: FC<Props> = ({
+  variant = 'default',
+  className,
+  loading,
+  disabled,
+  children,
+  icon,
+  ...props
+}) => {
+  return (
+    <button
+      {...props}
+      disabled={loading || disabled}
+      className={clsx(styles.mcButton, styles[variant], className)}
+    >
+      {loading ? (
+        <span className={styles.spinner} />
+      ) : (
+        icon && <span className={styles.icon}>{icon}</span>
+      )}
+      {children}
+    </button>
+  )
 }
