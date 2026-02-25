947f67f028193dc676706896d5f1d44ebaf80016
TinGoll <roma.oliynyk83@gmail.com>
2026-02-25 12:28:29 +0300

    prepare-fix

--- tin-craft/src/renderer/src/main.tsx
+++ tin-craft/src/renderer/src/main.tsx
@@ -1,13 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import { App } from './App'
 import '../src/assets/normalize.css'
 import '../src/assets/base.css'
 import '../src/assets/fonts.css'
 import { SWRProvider } from './providers'
-// import App1 from './App1'
+import App1 from './App1'
 
 createRoot(document.getElementById('root')!).render(
   <SWRProvider>
-    <App />
+    <App1 />
   </SWRProvider>
 )
